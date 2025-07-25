36ae2c2f2a6f7edb32bcbfea1f3698f424b05bcf
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-07-25 00:00:39 -0600

    Starting to edit the home screen

--- server/frontend/src/components/Header/header.jsx
+++ server/frontend/src/components/Header/header.jsx
@@ -3,11 +3,11 @@ import "./header.css";
 function header() {
     return (
         <>
-            <div className="header">
-                <div className="header-content">
-                    <h1 className="header-logo">MediRecord</h1>
-                    <div className="header-options">
-                        <p className="header-text">Benfits</p>
+            <div id="header">
+                <div id="header-content">
+                    <h1 id="header-logo">MediRecord</h1>
+                    <div id="header-options">
+                        <p className="header-text">Benefits</p>
                         <p className="header-text">Functions</p>
                         <p className="header-text">Contact</p>
                     </div>
--- server/frontend/src/home.jsx
+++ server/frontend/src/home.jsx
@@ -6,7 +6,21 @@ function Home() {
   return (
     <>
       <Header />
-      <h1>Hello World!</h1>
+      <div id='home-container'>
+        <div className='home-content' id="home-left-content">
+            <h1>Medical Records Management</h1>
+            <p>
+              Secure platform to manage medical records with artificial 
+              intelligence support for diagnostics and reporting.
+            </p>
+            <div id='home-content-buttons'>
+              <a className="home-button" id="home-button-green" href="">Sign Up</a>
+              <a className="home-button" id="home-button-white" href="">Sign In</a>
+            </div>
+        </div>
+        <div className='home-content' id="home-right-content">
+        </div>
+      </div>
     </>
   )
 }
