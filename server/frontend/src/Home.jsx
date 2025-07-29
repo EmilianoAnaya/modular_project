c3dc73744a7589a630c5e98f6134a81b28473d8f
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-07-28 20:44:46 -0600

    Completing the Sign In screen

--- server/frontend/src/Home.jsx
+++ server/frontend/src/Home.jsx
@@ -4,6 +4,7 @@ import './styles/Home.css'
 import Header from './components/Header/Header'
 import HomeContent from './components/Home_Content/HomeContent'
 import SignUp from './components/Sign_Up_In/SignUp'
+import SignIn from './components/Sign_Up_In/SignIn'
 
 function Home() {
   const [currentView, setCurrentView] = useState("home")
@@ -14,6 +15,8 @@ function Home() {
       <div id="home-container">
         {currentView === 'home' &&  <HomeContent setCurrentView={setCurrentView}/>}
         {currentView === 'sign_up' && <SignUp setCurrentView={setCurrentView}/>}
+        {currentView === 'sign_in' && <SignIn setCurrentView={setCurrentView}/>}
+
       </div>
     </>
   )
--- server/frontend/src/components/Sign_Up_In/SignUp.jsx
+++ server/frontend/src/components/Sign_Up_In/SignUp.jsx
@@ -1,10 +1,11 @@
 import './SignUp.css';
+import './SignShared.css';
 
 function SignUp({ setCurrentView }) {
     return (
         <>
             <div id='sign-up-content'>
-                <h1 className="sing-up-in-header">Sign Up</h1>
+                <h1 className="sig-up-in-header">Sign Up</h1>
                 <form>
                     <div id='sign-up-inputs-container'>
                         <input className="si-up-in-inputs" type='text' placeholder='Name'/>
@@ -16,9 +17,9 @@ function SignUp({ setCurrentView }) {
                         <input className="si-up-in-inputs" type="password" placeholder='Password'/>
                         <input className="si-up-in-inputs" type="password" placeholder='Confirm Password'/>
                     </div>
-                    <div id='si-up-butt-cont'>
+                    <div className='si-up-butt-cont'>
                         <button className='si-up-in-buttons' onClick={() => setCurrentView('home')}>Cancel</button>
-                        <button className='si-up-in-buttons' onClick={() => setCurrentView('sign-in')}>Register</button>
+                        <button className='si-up-in-buttons' onClick={() => setCurrentView('sign_in')}>Register</button>
                     </div>
                 </form>
             </div>
