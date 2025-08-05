c6fa19c6decdcd512a2988e3245fd01af882e22e
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-08-05 00:02:34 -0600

    JK now the Dashboard Patients screen is done for good

--- server/frontend/src/components/Dash_Patients/DashboardPatients.jsx
+++ server/frontend/src/components/Dash_Patients/DashboardPatients.jsx
@@ -1,6 +1,7 @@
 import './DashboardPatients.css'
 import Section from '../Section/Section'
 import Separator from '../Separator/Separator'
+import GridPatientsContent from './GridPatientsContent'
 
 function DashboardPatients(){
     return (
@@ -16,26 +17,15 @@ function DashboardPatients(){
             </Section>
             <Separator />
             <div className='basic-container patients-cont'>
-                <div id='patients-grid'>
-                    <div className='patients-row'>
-                        <div>Patient Name</div>
-                        <div>Last time opened by you</div>
-                        <div>Last time updated</div>
-                        <div>Actions</div>
-                    </div>
-                    <div className='patients-row patients-content'>
-                        <div className='patients-content-1st'>Patient Name</div>
-                        <div>xx/xx/xxxx</div>
-                        <div className='patients-content-3st'>
-                            <span>xx/xx/xxxx</span>
-                            <button className='basic-button icon-button table-button'>
-                                <img src='/assets/file-check-2.svg' />
-                            </button>
-                        </div>
-                        <div>
-                            <button className='basic-button table-button'>Consult</button>
-                        </div>
-                    </div>
+                <div className='patients-grid'>
+                    <div className='patients-grid-header'>Patient Name</div>
+                    <div className='patients-grid-header'>Last time opened by you</div>
+                    <div className='patients-grid-header'>Last time updated</div>
+                    <div className='patients-grid-header'>Actions</div>
+                    
+                    {/* Agregar rows por medio de este componente! */}
+                    <GridPatientsContent />
+                    
                 </div>
             </div>
         </>
---
+++ server/frontend/src/components/Dash_Patients/GridPatientsContent.jsx
@@ -0,0 +1,25 @@
+import './DashboardPatients.css'
+
+function GridPatientsContent(){
+    return(
+        <>
+            <div className='patients-grid-content patients-column-1'>
+                <span>Gael Emiliano Anaya Garcia</span>
+            </div>
+            <div className='patients-grid-content'>
+                <span>xx/xx/xxxx</span>
+            </div>
+            <div className='patients-grid-content patients-column-3'>
+                <span>xx/xx/xxxx</span>
+                <button className='basic-button icon-button table-button'>
+                    <img src='/assets/file-check-2.svg'/>
+                </button>
+            </div>
+            <div className='patients-grid-content'>
+                <button className='basic-button table-button'>Consult</button>
+            </div>
+        </>
+    )
+}
+
+export default GridPatientsContent
