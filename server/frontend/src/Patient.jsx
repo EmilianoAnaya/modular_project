326f6d0eacc75f492f1a20affd22d7b22297c59e
Emma Valeria <maemmva@outlook.com>
2025-08-27 20:50:08 -0600

    Add navigation bar and routes for patient section

--- server/frontend/src/Patient.jsx
+++ server/frontend/src/Patient.jsx
@@ -1,5 +1,8 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import MainNavbar from "./components/Main_Navbar/MainNavbar"
 import PatientSidebar from "./components/Patient_Sidebar/PatientSidebar"
+import PatientNotes from "./components/Patient_Notes/PatientNotes"
+import PatientNav from "./components/Patient_Navbar/PatientNav"
 import "./styles/Patient.css"
 
 function Patient(){
@@ -9,7 +12,11 @@ function Patient(){
             <div className="main-container patient-container">
                 <PatientSidebar />
                 <div className="patient-content">
-                    hello
+                    <PatientNav />
+                    <Routes>
+                        <Route path="notes" element={<PatientNotes />} />
+                        <Route path="/" element={<div>Bienvenido al paciente</div>} />
+                    </Routes>
                 </div>
             </div>
         </>
