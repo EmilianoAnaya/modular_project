d75d1061011be7bdb98c30cbaa3f74b503797dcf
Santino <santinobanana@gmail.com>
2025-09-02 18:05:13 -0600

    Hash.py es para pruebas de hasheo, a futuro quitar

--- server/backend/hash.py
+++ server/backend/hash.py
@@ -7,3 +7,6 @@ print("Hash generado:", hashed.decode('utf-8'))
 # Verificar que el hash funciona
 test = bcrypt.checkpw(password.encode('utf-8'), hashed)
 print("Verificación:", test)
+
+
+#Este código es para probar los hash de encriptación de contraseñas, a futuro eliminar
