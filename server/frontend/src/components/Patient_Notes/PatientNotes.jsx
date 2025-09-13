10ed2cef934821ad63b3a7677d14f94d0e53f343
Emma Valeria <maemmva@outlook.com>
2025-09-13 11:47:30 -0600

    Add editable notes card with save button

--- server/frontend/src/components/Patient_Notes/PatientNotes.jsx
+++ server/frontend/src/components/Patient_Notes/PatientNotes.jsx
@@ -5,6 +5,27 @@ import "./PatientNotes.css"
 function PatientNotes() {
   const [activeSection, setActiveSection] = useState('Allergies')
 
+  // Notas simuladas (pruebas)
+  const [notes, setNotes] = useState({
+    Allergies: "",
+    "Surgical History" : "",
+    "Habits" : "",
+    "Family History" : "",
+    "Chronic Diseases" : "",
+  })
+
+  const handleChange = (e) => {
+    setNotes({
+      ...notes,
+      [activeSection]: e.target.value, // Save the editable content
+    })
+  }
+
+  const handleSave = () => {
+    // Guardamos info en local
+    alert(`Nota guardada en sección "${activeSection}": \n${notes[activeSection]}`)
+  }
+
   return (
     <div className="notes-page">
         <PatientNav 
@@ -15,13 +36,12 @@ function PatientNotes() {
       <div className="notes-container">
         <div className="notes-card">
           <h1>{activeSection}</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam, unde, tempora ipsa nobis, ab non suscipit explicabo aliquam ullam dolorem libero! Explicabo aperiam maxime provident magnam doloribus saepe minus excepturi.</p>
-          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur, voluptas aliquid! Iste, dolor accusantium quam molestias, debitis obcaecati, ullam nobis magnam voluptates temporibus maxime beatae delectus recusandae est omnis qui.</p>
-          <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iusto quaerat nihil praesentium error, asperiores veritatis ut modi fuga facilis. Error saepe tempore quidem a! Placeat dicta molestiae pariatur eos rem.</p>
-          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam asperiores veniam corporis atque debitis quidem. Sint rerum fugiat possimus! Dolores sed, nostrum similique voluptas doloribus aspernatur veniam debitis quis totam.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Architecto dolore deserunt cum, similique vel neque sit. Ea rem consectetur vitae, dolor deserunt dolores perspiciatis reprehenderit at magnam sit hic minus?</p>
-          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae at, culpa eaque, totam exercitationem aliquam aut beatae alias debitis sunt maxime harum neque. Exercitationem similique nulla consectetur perspiciatis architecto qui?</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum architecto temporibus aliquid perferendis corrupti animi ipsam, voluptates eum illo in provident iste culpa omnis nesciunt dolorum molestias. Earum, consectetur corporis?</p>
+          
+          <div className='editable-note' contentEditable={true} onInput={handleChange} suppressContentEditableWarning={true}>
+            {notes[activeSection]}
+          </div>
+          <button className="save-button" onClick={handleSave}>Save</button>
+          
         </div>
       </div>
     </div>
