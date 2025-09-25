fc0e32d8d07eafcf59cade5f27cdcbabe9e9ef90
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-09-24 20:33:50 -0600

    Containers ready for Patient Consult screen and also modifying the Header component to allow changes like sets

--- server/frontend/src/components/Patient_Consult/PatientConsult.jsx
+++ server/frontend/src/components/Patient_Consult/PatientConsult.jsx
@@ -1,9 +1,30 @@
+import Heading from '../Heading/Heading'
 import './PatientConsult.css'
 
 function PatientConsult(){
     return (
         <>
-            Consult Page
+            <div className='patient-consult-container'>
+                <div className='consult-tools con-tls-1'>
+                    <div className='consult-box con-vitals'>
+                        <Heading headingText={"Vitals"} />
+                    </div>
+                    <div className='consult-box con-problems'>
+                        <Heading headingText={"Problems"} trigger={"problems-window"}/>
+                    </div>
+                </div>
+                <div className='consult-tools con-tls-2'>
+                    <div className='consult-box con-allergies'>
+                        <Heading headingText={"Allergies"} trigger={"allergies-window"}/>
+                    </div>
+                    <div className='consult-box con-medicines'>
+                        <Heading headingText={"Medicines"} trigger={"medicines-window"}/>
+                    </div>
+                    <div className='consult-box con-notes'>
+                        <Heading headingText={"Notes"} />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
