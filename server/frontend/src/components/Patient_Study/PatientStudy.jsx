b8b1332a63d9f648ac4a59ef79e28ee2b24c6f9c
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-10-11 20:26:12 -0600

    Working with the Patient Study view, almost finishing the Search Study container

--- server/frontend/src/components/Patient_Study/PatientStudy.jsx
+++ server/frontend/src/components/Patient_Study/PatientStudy.jsx
@@ -3,7 +3,56 @@ import './PatientStudy.css'
 function PatientStudy(){
     return(
         <>
-            Patient Study
+            <div className='patient-study-container'>
+                <div className='studies-list-cont'>
+                    <div className='studies-list'>
+                        <div className='studies-list-search'>
+                            <input placeholder='Search for Study...' />
+                            <div />
+                        </div>
+                        <div className='studies-list-items'>
+                            <ul>
+                                <li>
+                                    <div className='study-item'>
+                                        <span>X Rays</span>
+                                        <button>Add</button>
+                                    </div>
+                                </li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                                <li></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div className='studies-selected-cont'></div>
+            </div>
         </>
     )
 }
