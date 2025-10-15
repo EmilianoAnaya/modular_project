e1abef0eb0e2829305edacea6accd53730f72990
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-10-15 17:14:26 -0600

    Finishing the Patient Study screen!

--- server/frontend/src/components/Patient_Study/StudyCard.jsx
+++ server/frontend/src/components/Patient_Study/StudyCard.jsx
@@ -2,25 +2,39 @@ import './StudyCard.css'
 import { useState } from 'react'
 
 function StudyCard({ backgroundImage }){
-    // const StudyCardBackground = backgroundImage
     
+    // const StudyCardBackground = backgroundImage
     const studyCardBackground = '/assets/XRaysExample.jpg'
-    const [cardState, setCardState] = useState(0)
+
+    const [cardState, setCardState] = useState(false)
+    const [showItems, setShowItems] = useState(false);
 
     return(
         <>
-            <div className='study-card-container' >
+            <div className='study-card-container' onMouseOver={() => setShowItems(true)} onMouseLeave={() => setShowItems(false)}>
                 <div 
                     className='study-card-background' 
                     style={{ backgroundImage : `url(${studyCardBackground})`}} 
                 />
-                {/* <img src='/assets/XRaysExample.jpg' className='study-card-image' /> */}
 
-                <img src="/assets/cross.svg" className='study-card-button study-card-exit-button' />
-                {/* <img src='/assets/pen.svg' className='study-card-button study-card-edit-button'/> */}
+                {showItems && (
+                    <>  
+                        {!cardState ? (
+                            <>
+                                <img src='/assets/paperclip.svg' className='study-card-option' />
+                            </>
+                        ) : (
+                            <>
+                                <img src='/assets/glasses.svg' className='study-card-option' />
+                                <img src='/assets/pen.svg' className='study-card-button study-card-edit-button'/>
+                            </>
+                        )}
+                        <img src="/assets/cross.svg" className='study-card-button study-card-exit-button' />
+                    </>
+                )}
 
-                <img src='/assets/paperclip.svg' className='study-card-option' />
-                {/* <img src='/assets/glasses.svg' className='study-card-option' /> */}
+
+
             </div>
         </>
     )
