d176d0ebb3aed011682076b4a1ecd30f6df64037
EmilianoAnaya <gael.anaya@alumnos.udg.mx>
2025-10-15 14:38:47 -0600

    Finishing with the style of the Study Cards, now working with its hover logic and states

--- server/frontend/src/components/Patient_Study/StudyCard.jsx
+++ server/frontend/src/components/Patient_Study/StudyCard.jsx
@@ -1,10 +1,26 @@
 import './StudyCard.css'
+import { useState } from 'react'
 
-function StudyCard(){
+function StudyCard({ backgroundImage }){
+    // const StudyCardBackground = backgroundImage
+    
+    const studyCardBackground = '/assets/XRaysExample.jpg'
+    const [cardState, setCardState] = useState(0)
+
     return(
         <>
-            <div className='study-card-container'>
-                <img />
+            <div className='study-card-container' >
+                <div 
+                    className='study-card-background' 
+                    style={{ backgroundImage : `url(${studyCardBackground})`}} 
+                />
+                {/* <img src='/assets/XRaysExample.jpg' className='study-card-image' /> */}
+
+                <img src="/assets/cross.svg" className='study-card-button study-card-exit-button' />
+                {/* <img src='/assets/pen.svg' className='study-card-button study-card-edit-button'/> */}
+
+                <img src='/assets/paperclip.svg' className='study-card-option' />
+                {/* <img src='/assets/glasses.svg' className='study-card-option' /> */}
             </div>
         </>
     )
